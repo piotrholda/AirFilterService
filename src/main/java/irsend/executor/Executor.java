dbe8de0e5802484064261672ef11bc60c4fa29cb
Piotrek <pholda+github@gmail.com>
2019-01-14 19:07:14 +0100

    Remove commented code

--- src/main/java/irsend/executor/Executor.java
+++ src/main/java/irsend/executor/Executor.java
@@ -7,17 +7,6 @@ import java.util.List;
 
 public class Executor {
 
-/*
-    public static void main(String... args) throws IOException, InterruptedException {
-        List<String> codes = Arrays.asList(args);
-        Executor executor = new Executor();
-        int exitCode = executor.execute("klarta_forste_2", codes);
-        if (exitCode != 0) {
-            System.err.println("Command executed with exit code: " + exitCode);
-        }
-    }
-*/
-
     public int execute(String device, Collection<String> codes) throws InterruptedException, IOException {
         List<String> commands = new ArrayList<>();
         commands.add("irsend");
