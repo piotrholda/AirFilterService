aa69d94b78f00f742782f6e121499b90ca5ba9ac
Piotrek <pholda+github@gmail.com>
2019-01-18 15:38:01 +0100

    Unit test for application context.

---
+++ src/test/java/airfilter/service/ApplicationTest.java
@@ -0,0 +1,49 @@
+package airfilter.service;
+
+import airly.client.AirlyProperties;
+import airly.client.ApplicationProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+    @Autowired
+    AirlyProperties airlyProperties;
+
+    @Test
+    public void contextShouldStart() {
+    }
+
+    @Test
+    public void urlPropertyShouldBeRead() {
+        assertThat(applicationProperties.getApiUrl()).isNotEmpty();
+    }
+
+    @Test
+    public void limitPropertiesShouldBeRead() {
+        assertThat(applicationProperties.getLimitDay()).isGreaterThan(0);
+        assertThat(applicationProperties.getLimitMinute()).isGreaterThan(0);
+    }
+
+    @Test
+    public void installationIdsPropertyShouldBeRead() {
+        assertThat(applicationProperties.getInstallationIds()).isNotEmpty();
+        assertThat(applicationProperties.getInstallationIds().get(0)).isGreaterThan(0);
+    }
+
+    @Test
+    public void keyPropertyShouldBeRead() {
+        assertThat(airlyProperties.getKey()).isNotEmpty();
+    }
+
+}
